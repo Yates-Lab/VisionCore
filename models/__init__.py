fe674ff39eb1d3f8b0b6c6f2d501448f43f35272
Jacob Yates <yates@berkeley.edu>
2025-10-08 15:50:49 -0700

    refactor to avoid dead config code

--- models/__init__.py
+++ models/__init__.py
@@ -5,12 +5,7 @@ This package contains neural network models for neural data analysis.
 """
 
 from .build import build_model, initialize_model_components, get_name_from_config
-from .config import (
-    build_model_config,
-    build_component_config,
-    merge_configs,
-    get_component_defaults
-)
+from .config import build_component_config
 from .checkpoint import (
     load_model_from_checkpoint,
     find_best_checkpoint,
