c0fec6992e13bebbe010137122db5c820c8de945
Jacob Yates <yates@berkeley.edu>
2026-01-09 09:55:49 -0800

    beginning refactor on spatial information code

--- scripts/check_fixrsvp_model_spatialinfo.py
+++ scripts/check_fixrsvp_model_spatialinfo.py
@@ -32,6 +32,12 @@ model.model.eval()
 model.model.convnet.use_checkpointing = True  # Enable checkpointing to save GPU memory
 model = model.to(device)
 
+import dill
+with open('mcfarland_outputs.pkl', 'rb') as f:
+    outputs = dill.load(f)
+
+sessions = [outputs[i]['sess'] for i in range(len(outputs))]
+#%%
 dataset_idx = 10
 print(f"Loading dataset {dataset_idx}: {model.names[dataset_idx]}")
 train_data, val_data, dataset_config = load_single_dataset(model, dataset_idx)
@@ -61,7 +67,11 @@ frames_per_im = 6
 full_stack = get_fixrsvp_stack(frames_per_im=frames_per_im)
 print(f"Full stimulus stack shape: {full_stack.shape}")
 
+#%%
+model_dataset_idx = [(i, model.names[i]) for i, name in enumerate(model.names) if name in sessions]
+
 #%% Helper functions
+import torch.nn.functional as F
 def embed_time_lags(movie, n_lags=32):
     """
     Embed time lags into a movie tensor.
@@ -122,6 +132,15 @@ def run_model(model, stim, batch_size=32):
     T = stim.shape[0]
     y_chunks = []
 
+    W = stim.shape[-1]
+    model.model.adapters[0].grid_size = W
+    # ---- static base grid  (-1 … +1) -------------------------------- #
+    lin = torch.linspace(-1.0, 1.0, W)
+    gy, gx = torch.meshgrid(lin, lin, indexing="ij")
+    base = torch.stack((gx, gy), -1)  # [g,g,2]
+    model.model.adapters[0].register_buffer("base_grid", base, persistent=False)
+    
+    model.model.eval()
     with torch.no_grad():
         for t_start in range(0, T, batch_size):
             t_end = min(t_start + batch_size, T)
@@ -129,13 +148,26 @@ def run_model(model, stim, batch_size=32):
             # Move batch to GPU
             stim_gpu = stim[t_start:t_end].to(device)
 
-            x = run_core(model.model, stim_gpu, None)
+            x = stim_gpu
+            # x = model.model.adapters[0](stim_gpu)
+
+            x = run_core(model.model, x, None)
             del stim_gpu
 
             units = []
-            for readout in model.model.readouts:
+            for i, name in model_dataset_idx:
+                readout = model.model.readouts[i]
+                output_idx = sessions.index(name)
+                cids2use = np.where(outputs[output_idx]['ccnorm']['ccnorm']>.5)[0]
+
                 readout.eval()
-                units.append(readout.features(x[:, :, -1]))
+                
+                mask = readout.compute_gaussian_mask(14, 14, device)
+                
+                feat = readout.features(x[:, :, -1])
+                space = torch.nn.functional.conv2d(feat, mask[:, None, :, :], groups=readout.n_units, padding="valid")
+                space = space + readout.bias[None, :, None, None]
+                units.append(space[:, cids2use])
 
             del x
             y_batch = model.model.activation(torch.cat(units, dim=1))
@@ -196,25 +228,36 @@ def make_movie(y, save_path='', n_units_to_show=100):
     print(f"Saved spatial activations movie to {save_path}")
 
 # Reconstruct stimulus
-def make_counterfactual_stim(eyepos, type='fixrsvp', frame = None, scale_factor = 1.0, n_lags = 32, out_size = (101, 101)):
-    ppd = 37.50476617
-    frames_per_im = 6
+def make_counterfactual_stim(eyepos, type='fixrsvp',
+                            frame = None, 
+                            frames_per_im = 6,
+                            ppd = 37.50476617,
+                            scale_factor = 1.0,
+                            n_lags = 32,
+                            out_size = (101, 101)):
+    '''
+    Reconstruct stimulus from eye positions.
+    
+    Input:
+        eyepos: [T, 2] eye positions in degrees
+        type: 'fixrsvp', 'face', 'nat'
+        frame: frame number to use for all time points (None flashes frames at framerate specified by frames_per_im)
+        frames_per_im: number of frames to show each image for (if frame is None)
+        ppd: pixels per degree
+        scale_factor: scale factor for stimulus (1.0 is no scaling)
+        n_lags: number of time lags to use
+        out_size: (H, W) size of output stimulus
+    '''
     
     if type == 'fixrsvp':
-        frames_per_im = 6
         full_stack = get_fixrsvp_stack(frames_per_im=frames_per_im, prefix='im')
     elif type == 'face':
-        frames_per_im = 6
         full_stack = get_fixrsvp_stack(frames_per_im=frames_per_im, prefix='face')
     elif type == 'nat':
-        frames_per_im = 1
         full_stack = get_fixrsvp_stack(frames_per_im=frames_per_im, prefix='nat')
 
-    print(f"Full stimulus stack shape: {full_stack.shape}")
     if frame is not None:
         full_stack = full_stack[[frame]].repeat(eyepos.shape[0]+n_lags*2, axis=0)
-    print(f"Full stimulus stack shape: {full_stack.shape}")
-    print(f"fullstack data type: {full_stack.dtype}")
 
     eye_norm = eye_deg_to_norm(torch.fliplr(eyepos), ppd, full_stack.shape[1:3])
 
@@ -236,7 +279,7 @@ torch.cuda.empty_cache()
 
 #%% flashed stimulus
 
-n_lags = 64
+
 out_size = (151, 151)
 dt = 1/120
 
@@ -252,8 +295,9 @@ ix = (trials[itrial] == trial_inds) & fixation
 stim_inds_orig = np.where(ix)[0]
 
 # frame += 6
-frame = 4
-type = 'nat'
+frame = 32
+n_lags = 32
+type = 'fixrsvp'
 scale = 1.0
 
 eyepos = dataset.dsets[dset_idx]['eyepos'][ix]
@@ -274,19 +318,34 @@ y = run_model(model, eye_stim)
 y_null = run_model(model, eye_stim_null)
 
 # compute spatial info
-ispike, irate, I_t = spatial_ssi_population(y, dt=dt)
-ispike_null, irate_null, I_t_null = spatial_ssi_population(y_null, dt=dt)
+ispike, irate, I_t = spatial_ssi_population(y)
+ispike_null, irate_null, I_t_null = spatial_ssi_population(y_null)
 
 plt.plot(eyepos.numpy())
 plt.show()
 
-for cc in [0, 5, 10, 20, 100, 500]:
+inds = np.argsort(I_t.mean(0).numpy()-I_t_null.mean(0).numpy())[::-1]
+
+for cc in inds[:10]:
     plt.figure()
-    plt.subplot(2,1,1)
-    plt.plot(y[:,cc,0,::5])
-    plt.subplot(2,1,2)
+    plt.subplot(3,1,1)
+    plt.plot(y[:,cc,0,::5]/dt, 'b')
+    plt.plot(y_null[:,cc,0,::5]/dt, 'r')
+    plt.title(f'Unit {cc}')
+    plt.ylabel('Rate (spikes/bin)')
+    plt.subplot(3,1,2)
     plt.plot(I_t[:,cc])
     plt.plot(I_t_null[:,cc])
+    plt.xlabel('Frame')
+    plt.ylabel('Spatial Info (bits)')
+    plt.subplot(3,1,3) # plot variance across space
+    plt.plot(y[:,cc].var((1,2)), 'b--')
+    plt.plot(y_null[:,cc].var((1,2)), 'r--')
+    plt.plot(y[:,cc].mean((1,2)), 'b')
+    plt.plot(y_null[:,cc].mean((1,2)), 'r')
+    plt.xlabel('Frame')
+    plt.ylabel('Variance across space')
+    plt.show()
 
 
 #%%
@@ -298,7 +357,7 @@ plt.figure()
 _ = plt.plot(I_t.mean(0), I_t_null.mean(0), '.', alpha=0.1)
 plt.plot(plt.xlim(), plt.xlim(), 'k')
 plt.xlabel('Spatial Info (Real FEMs)')
-plt.ylabel('Spatial Info (No stim)')
+plt.ylabel('Spatial Info (No FEMs)')
 plt.title('Spatial Info (Units)')
 plt.show()
 
@@ -309,7 +368,7 @@ plt.xlabel('Time (frames)')
 plt.legend(['Real stim', 'Null stim'])
 plt.title(f'Cumulative Spatial Info (population) {frame}')
 # %%
-units_to_show = np.argsort(I_t.mean(0)-I_t_null.mean(0)).numpy()[::-1][0:1000:50]
+units_to_show = np.argsort(I_t.mean(0)-I_t_null.mean(0)).numpy()[::-1][:25]
 
 if frame is None:
     make_movie(y, save_path='counterfactual1', n_units_to_show=units_to_show)
@@ -345,4 +404,166 @@ plt.colorbar()
 
 
 
-# %%
+# %% Loop over trials and compute spatial information on the real stimulus
+from tqdm import tqdm
+frame = None # flashed
+type = 'fixrsvp' # fixrsvp stim
+scale = 1.0 # normal scale
+
+ispikes = []
+irates = []
+ispikes_null = []
+irates_null = []
+I_t_list = []
+I_t_null_list = []
+
+for itrial in tqdm(trial_list[:70]):
+    ix = (trials[itrial] == trial_inds) & fixation
+    if np.sum(ix) < 64:
+        continue
+    stim_inds_orig = np.where(ix)[0]
+
+    eyepos = dataset.dsets[dset_idx]['eyepos'][ix]
+    null_eyepos = torch.zeros_like(eyepos) + eyepos.mean(0)
+    eye_stim = make_counterfactual_stim(eyepos, type=type,
+        frame=frame, out_size=out_size, n_lags=n_lags, scale_factor=scale)
+    eye_stim_null = make_counterfactual_stim(null_eyepos, type=type,
+        frame=frame, out_size=out_size, n_lags=n_lags, scale_factor=scale)
+
+    y = run_model(model, eye_stim)
+    y_null = run_model(model, eye_stim_null)
+
+    # compute spatial info
+    ispike, irate, I_t = spatial_ssi_population(y)
+    ispike_null, irate_null, I_t_null = spatial_ssi_population(y_null)
+
+    ispikes.append(ispike)
+    irates.append(irate)
+    ispikes_null.append(ispike_null)
+    irates_null.append(irate_null)
+    I_t_list.append(I_t)
+    I_t_null_list.append(I_t_null)
+
+    # v = out_size[0]/ppd
+    # plt.imshow(eye_stim[0,0,0].numpy(), cmap='gray', extent=[-v/2, v/2, -v/2, v/2])
+    # plt.plot(eyepos[:,0].numpy(), eyepos[:,1].numpy(), 'r')
+    # plt.show()
+
+#%%
+
+plt.subplot(1,2,1)
+plt.plot(np.array(ispikes_null), np.array(ispikes), '.')
+plt.plot(plt.xlim(), plt.xlim(), 'k')
+plt.xlabel('Bits/Spike(Null stim)')
+plt.ylabel('Bits/Spike (Real stim)')
+plt.title('Spatial Info (Units)')
+
+plt.subplot(1,2,2)
+plt.plot(np.array(irates_null), np.array(irates), '.')
+plt.plot(plt.xlim(), plt.xlim(), 'k')
+plt.xlabel('Spatial Info Rate (Null stim)')
+plt.ylabel('Spatial Info Rate (Real stim)')
+plt.title('Spatial Info (Units)')
+
+
+#%%
+
+type = 'nat' # natural images
+scale = 1.0 # normal scale
+
+ispikes = []
+irates = []
+ispikes_null = []
+irates_null = []
+I_t_list = []
+I_t_null_list = []
+
+for itrial in tqdm(trial_list[:70]):
+    ix = (trials[itrial] == trial_inds) & fixation
+    if np.sum(ix) < 64:
+        continue
+    stim_inds_orig = np.where(ix)[0]
+
+    eyepos = dataset.dsets[dset_idx]['eyepos'][ix]
+    null_eyepos = torch.zeros_like(eyepos) + eyepos.mean(0)
+
+    for frame in range(32):
+        eye_stim = make_counterfactual_stim(eyepos, type=type,
+            frame=frame, out_size=out_size, n_lags=n_lags, scale_factor=scale)
+        eye_stim_null = make_counterfactual_stim(null_eyepos, type=type,
+            frame=frame, out_size=out_size, n_lags=n_lags, scale_factor=scale)
+
+        y = run_model(model, eye_stim)
+        y_null = run_model(model, eye_stim_null)
+
+        # compute spatial info
+        ispike, irate, I_t = spatial_ssi_population(y)
+        ispike_null, irate_null, I_t_null = spatial_ssi_population(y_null)
+
+        ispikes.append(ispike)
+        irates.append(irate)
+        ispikes_null.append(ispike_null)
+        irates_null.append(irate_null)
+        I_t_list.append(I_t)
+        I_t_null_list.append(I_t_null)
+
+    # v = out_size[0]/ppd
+    # plt.imshow(eye_stim[0,0,0].numpy(), cmap='gray', extent=[-v/2, v/2, -v/2, v/2])
+    # plt.plot(eyepos[:,0].numpy(), eyepos[:,1].numpy(), 'r')
+    # plt.show()
+
+#%%
+
+plt.subplot(1,2,1)
+plt.plot(np.array(ispikes_null), np.array(ispikes), '.')
+plt.plot(plt.xlim(), plt.xlim(), 'k')
+plt.xlabel('Bits/Spike(Null stim)')
+plt.ylabel('Bits/Spike (Real stim)')
+plt.title('Spatial Info (Units)')
+
+plt.subplot(1,2,2)
+plt.plot(np.array(irates_null), np.array(irates), '.')
+plt.plot(plt.xlim(), plt.xlim(), 'k')
+plt.xlabel('Spatial Info Rate (Null stim)')
+plt.ylabel('Spatial Info Rate (Real stim)')
+plt.title('Spatial Info (Units)')
+
+
+
+
+
+#%% Now do it over all sessions...
+
+for sess in sessions:
+    dataset_idx = model.names.index(sess)
+    print(f"Loading dataset {dataset_idx}: {model.names[dataset_idx]}")
+    train_data, val_data, dataset_config = load_single_dataset(model, dataset_idx)
+
+    # Get fixrsvp trial indices
+    inds = torch.concatenate([
+        train_data.get_dataset_inds('fixrsvp'),
+        val_data.get_dataset_inds('fixrsvp')
+    ], dim=0)
+
+    dataset = train_data.shallow_copy()
+    dataset.inds = inds
+
+    dset_idx = inds[:,0].unique().item()
+    trial_inds = dataset.dsets[dset_idx].covariates['trial_inds'].numpy()
+    trials = np.unique(trial_inds)
+    NT = len(trials)
+
+    fixation = np.hypot(
+        dataset.dsets[dset_idx]['eyepos'][:,0].numpy(), 
+        dataset.dsets[dset_idx]['eyepos'][:,1].numpy()
+    ) < 1
+
+#%%
+
+y = run_model(model, eye_stim)
+y_null = run_model(model, eye_stim_null)
+
+# compute spatial info
+ispike, irate, I_t = spatial_ssi_population(y)
+ispike_null, irate_null, I_t_null = spatial_ssi_population(y_null)
+
